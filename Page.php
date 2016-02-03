7ce10ea0134836c73555a0a8d5f4c445c90a30aa
aisdata <aisdata@aisdata>
2016-02-03 17:20:51 +0200

    Page updated

--- Page.php
+++ Page.php
@@ -2,17 +2,69 @@
 
 namespace izosa\page;
 
+use voku\helper\HtmlDomParser;
+
+
+
 /**
  * Parcer
  * @author aisdata
  */
 class Page {
 
-    public $url;
+    private $url;
+    private $html;
+    private $dom = null;
+    private $handler;
     
-    public static function hey(){ 
-        return 'Tadaa';
+    public function __construct($url, $init = true, $proxy = []){
+        
+        $this->url = $url;
+        
+        if($init) $this->download($proxy);
     }
     
+    public function download($proxy = []){
+        $this->handler = curl_init();
+        curl_setopt($this->handler, CURLOPT_URL, $this->url);
+        
+        // proxy 
+        if(!empty($proxy))
+        {
+            curl_setopt($this->handler, CURLOPT_HTTPPROXYTUNNEL, 0);
+            curl_setopt($this->handler, CURLOPT_PROXY, $proxy['url']);
+        }
+        
+        curl_setopt($this->handler, CURLOPT_USERAGENT, random_uagent());
+        curl_setopt($this->handler, CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($this->handler, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($this->handler, CURLOPT_MAXREDIRS, 100);
+        curl_setopt($this->handler, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->handler, CURLOPT_CONNECTTIMEOUT, 5);
+        $this->html = curl_exec($this->handler);
+        curl_close($this->handler);
+    }
+
+    public function save($path){
+        $handler = fopen($path.DIRECTORY_SEPARATOR.str_replace("/", "|", $this->url), 'w+');
+        $fileSize = fwrite($handler, $this->html);
+        fclose($handler);
+        return $fileSize > 0;
+    }
     
+    public function find($query, $index = null){
+        if(is_null($this->dom)){
+            $this->dom = HtmlDomParser::str_get_html($this->html);
+        }
+        $this->dom->find($query,$index);
+    }
+    
+    public function getContent(){
+        return $this->html;
+    }
+
+    public function getUrl(){
+        return $this->html;
+    }
+
 }
